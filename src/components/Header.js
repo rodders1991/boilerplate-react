9cffa7ccfa1d278469f451470e1faa5d7fa35572
George <george@everledger.io>
2018-07-18 16:08:09 +0100

    Setup placeholder components for home page

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <p>This is the header</p>
+  </div>
+);
+
+export default Header;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import HomePage from '../components/HomePage';
+import Footer from '../components/Footer';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={HomePage} exact={true}/>
+      </Switch>
+    <Footer />
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
