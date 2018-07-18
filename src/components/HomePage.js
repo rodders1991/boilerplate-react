6a765c17929083d2b695c55a601c417a5e19e5f6
George <george@everledger.io>
2018-07-18 23:20:59 +0100

    Added images to article

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,18 @@ const HomePage = () => (
   <div>
     {/* Move to its own compnent once I have the gallery working */}
     <div className="home__gallery">
-      <img src="/images/cover.jpeg" width="100%" height="100%" />
+      <img src="/images/cover.jpg" width="100%" height="100%" />
     </div>
     <div className="home__content">
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
+      <div className="circle">
+        <img src="/images/article1.jpg" />
+      </div>
+      <div className="circle">
+        <img src="/images/article2.jpg" />
+      </div>
+      <div className="circle">
+        <img src="/images/article3.jpg" />  
+      </div>
     </div>
   </div>
 );
